95c44d292c5a1e4ec00d2dcce33b4116b01aa946
shridivyanshkotnala <divyansh.5119051924@std.ggsipu.ac.in>
2026-03-20 15:00:01 +0530

    Update forced test date to 12 Aug 2026

--- server/src/utils/dateTime.js
+++ server/src/utils/dateTime.js
@@ -1,6 +1,6 @@
 // TEMP TEST OVERRIDE: single source of time for FY rollover testing.
 // Revert to real-time after verification.
-const FORCED_TEST_DATE_ISO = "2026-04-02T00:00:00+05:30";
+const FORCED_TEST_DATE_ISO = "2026-08-12T00:00:00+05:30";
 
 export function getCurrentSystemDate() {
   return new Date(FORCED_TEST_DATE_ISO);
